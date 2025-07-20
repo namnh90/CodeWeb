5fad3092a4ac8041c92e0b9cb1921c5fd7204750
namnh90 <nam90.nguyenhoang@gmail.com>
2025-07-20 10:30:12 +0700

    Final bài tập về nhà 07

--- ls07.js
+++ ls07.js
@@ -33,4 +33,61 @@ function tinhtoan3() {
     
     document.getElementById("tinhgiaithua").value = ketqua
 }
+function tinhtoan4() {
+    let sotrai = 2
+    let sophai = 1
+    let bcc =""
+    for (let sotrai = 2; sotrai <= 9; sotrai++) {
+        for (let sophai = 1; sophai <=9; sophai++)
+        {   
+            let tich = Number(sophai)*Number(sotrai)
+            bcc += sotrai+'x'+sophai+'='+tich+'\n'
+        }
+         bcc += "\n"
+    }
+    document.getElementById("bcc").value = bcc
+}
 
+function tinhtoan5() {
+    let sobichia = document.getElementById("sobichia").value
+    let thuong = 1
+    let du = ""
+    for (;;) {
+        if (thuong > 0) { thuong=(sobichia-(sobichia%2))/2; du = sobichia%2+du; sobichia = (sobichia-(sobichia%2))/2 ;console.log(thuong)
+    }else{break;}}
+    document.getElementById("nhiphan").value = du
+}
+
+function tinhtoan6() {
+    let socantinh = document.getElementById("socantinh").value
+    if (!Number.isInteger(Number(socantinh))) { alert('Không phải là số nguyên'); return}
+    let socanlaycan = 1
+    can = 1
+    console.log("socantinh",socantinh)
+for (let i = 1; i <= Number(socantinh) + 1; i++)
+        {   
+            
+            can = Math.sqrt(socanlaycan)
+            socanlaycan = can + i
+            console.log("can",can)
+            console.log("socanlaycan",socanlaycan)
+        }
+    document.getElementById("tinhbai6").value = Number(can)
+}
+
+function tinhtoan7() {
+    let socantinh1 = document.getElementById("socantinh1").value
+    if (!Number.isInteger(Number(socantinh1))) { alert('Không phải là số nguyên'); return}
+    let socanlaycan = Number(socantinh1)
+    can = 1
+    console.log("socantinh1",socantinh1)
+for (let i = Number(socantinh1); i >= 0; i--)
+        {   
+            
+            can = Math.sqrt(socanlaycan)
+            socanlaycan = can + i
+            console.log("can",can)
+            console.log("socanlaycan",socanlaycan)
+        }
+    document.getElementById("tinhbai7").value = Number(can)
+}
